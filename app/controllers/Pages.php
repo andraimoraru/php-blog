ef469bb297ccf0e6010170e299d5ad3b9488bac5
Andra Moraru <andraimoraru@gmail.com>
2025-07-24 13:21:06 +0100

    pages, navbar, bootstrap

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -10,7 +10,8 @@
         public function index() {
 
             $data = [
-                'title' => 'Welcome to MVC',
+                'title' => 'Blog posts',
+                'description' => 'This is a simple blog application built with PHP MVC framework.'
             ];
             $this->view('pages/index', $data);
 
@@ -19,7 +20,8 @@
     
         public function about() {
             $data = [
-                'title' => 'About Us'
+                'title' => 'About Us',
+                'description' => 'App to share blogs with other users.'
             ];
             $this->view('pages/about', $data);
         }
