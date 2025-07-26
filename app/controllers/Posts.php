31c174af1d4a4ed35908566f788c621212ab39fe
Andra Moraru <andraimoraru@gmail.com>
2025-07-26 18:54:38 +0100

    post model and display

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -1,14 +1,21 @@
 <?php 
  class Posts extends Controller {
+     protected $postModel;
+
      public function __construct() {
         if (!isLoggedIn()) {
             redirect('users/login');
         }
+        $this->postModel = $this->model('Post');
 
      }
 
      public function index() {
-        $data = [];
+        // Get posts
+        $posts = $this->postModel->getPosts();
+        $data = [
+            'posts' => $posts
+        ];
          $this->view('posts/index', $data);
      }
  }
--- app/models/Post.php
+++ app/models/Post.php
@@ -7,8 +7,19 @@ class Post {
     }
     // Get all posts
     public function getPosts() {
-        $this->db->query('SELECT * FROM posts');
-        $results = $this->db->resultSet();
+        $this->db->query('SELECT * , 
+                        users.name AS name,
+                        posts.id AS postId,
+                        users.id AS userId,
+                        posts.body AS body,
+                        posts.title AS title,
+                        posts.created_at AS postCreatedAt                      
+                        FROM posts
+                        JOIN users 
+                        ON posts.user_id = users.id
+                        ORDER BY posts.created_at DESC');
+              
+        $results = $this->db->resultSet();      
         return $results;    
     }   
 }
